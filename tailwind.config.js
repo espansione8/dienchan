0b6f9e56eab60501bd40a937e0724ffd999c56e6
espansione8 <espansione8@gmail.com>
2025-03-01 22:21:40 +0100

    - prepare update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui";
 
 // do not use "export default { }" in guides https://tailwindcss.com/docs/guides/sveltekit define "module.exports = {}"
 module.exports = {
@@ -23,58 +22,4 @@ module.exports = {
     },
   },
 
-  plugins: [
-    daisyui,
-  ],
-  daisyui: {
-    //base: false, // uncomment to fix black background issue
-    //themes: true,
-    themes: [
-      {
-        dienchan: {
-          "primary": "#1a93dc",
-          "secondary": "#f50101",
-          "accent": "#292fa7",
-          // "neutral": "#1b263a",
-          // "base-100": "#fcfcff",
-          "info": "#0090e0",     // Darker and more brilliant blue
-          "success": "#00b050",  // Darker and more brilliant green
-          "warning": "#ff9900",  // Darker and more brilliant orange
-          "error": "#e81123",    // Darker and more brilliant red
-        },
-      },
-      // "light",
-      // "dark",
-      // "cupcake",
-      // "bumblebee",
-      // "emerald",
-      //  "corporate",
-      // "synthwave",
-      // "retro",
-      // "cyberpunk",
-      // "valentine",
-      // "halloween",
-      // "garden",
-      // "forest",
-      // "aqua",
-      // "lofi",
-      // "pastel",
-      // "fantasy",
-      // "wireframe",
-      // "black",
-      // "luxury",
-      // "dracula",
-      // "cmyk",
-      // "autumn",
-      // "business",
-      // "acid",
-      // "lemonade",
-      // "night",
-      // "coffee",
-      // "winter",
-      // "dim",
-      // "nord",
-      // "sunset",
-    ],
-  },
 };
