9d868e385add8d03c6a5d4fdb66da06a74291906
espansione8 <espansione8@gmail.com>
2025-12-15 22:34:27 +0200

    - add feb in course list in course shop

--- src/routes/(app)/course-shop/+page.server.ts
+++ src/routes/(app)/course-shop/+page.server.ts
@@ -15,9 +15,9 @@ export const load: PageServerLoad = async ({ fetch, locals }) => {
 		// get courses
 		const currentYear = new Date().getFullYear()
 		//const startOfYear = new Date(currentYear, 0, 1); //from Jan
-		const startOfYear = new Date(currentYear, 1, 1); // from feb
+		const startOfYear = new Date(currentYear, 2, 1); // from march
 		//const startOfNextYear = new Date(currentYear + 1, 0, 1); // start of 01 Jan
-		const startOfNextYear = new Date(currentYear + 1, 1, 1); // start of 01 Feb
+		const startOfNextYear = new Date(currentYear + 1, 2, 1); // start of 01 march
 
 		const resProductsCorso = await fetch(`${baseURL}/api/mongo/find`, {
 			method: 'POST',
